6e87284cec62dbcbfedb84ef67cfb1972b17fc56
Danyache <dchesakov@nes.ru>
2018-11-16 19:38:02 +0300

    Update bot_virtual.py

--- bot_virtual.py
+++ bot_virtual.py
@@ -5,8 +5,8 @@ from aiogram.utils import executor
 import requests
 from bs4 import BeautifulSoup
 import re
-from PIL import Image
-from io import BytesIO
+# from PIL import Image
+# from io import BytesIO
 
 
 
